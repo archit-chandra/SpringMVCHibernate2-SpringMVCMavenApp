35f07fde58375984997a9ce33b610e48490d13e0
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-25 23:30:07 +0200

    added controller method to process the customer form

--- src/main/java/com/gemalto/controllers/CustomerController.java
+++ src/main/java/com/gemalto/controllers/CustomerController.java
@@ -3,7 +3,11 @@ package com.gemalto.controllers;
 import com.gemalto.models.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/customer")
@@ -13,5 +17,14 @@ public class CustomerController {
     public String showForm(Model customerModel) {
         customerModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer customer, BindingResult result) {
+        if (result.hasErrors()) {
+            return "customer-form";
+        } else {
+            return "customer-confirmation";
+        }
     }
 }
