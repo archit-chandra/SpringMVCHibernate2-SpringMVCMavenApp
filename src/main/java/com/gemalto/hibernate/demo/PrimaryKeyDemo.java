65a6e9278c70a87f610b27558311dbc1e0cedec9
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-03-31 20:49:40 +0200

    hibernate - primary key generation strategy (custom strategy notes also)

--- src/main/java/com/gemalto/hibernate/demo/PrimaryKeyDemo.java
+++ src/main/java/com/gemalto/hibernate/demo/PrimaryKeyDemo.java
@@ -5,10 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class StudentDemo {
-
+public class PrimaryKeyDemo {
     public static void main(String[] args) {
-
         // create session factory
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -19,13 +17,17 @@ public class StudentDemo {
         Session session = factory.getCurrentSession();
 
         try {
-            // create a student object
-            System.out.println("Creating new student objects...");
-            Student student = new Student("Paul", "Wall", "paul@gamil.com");
+            // create 3 student objects
+            System.out.println("Creating 3 student objects...");
+            Student student1 = new Student("John", "Doe", "john@gamil.com");
+            Student student2 = new Student("Mary", "Public", "mary@gamil.com");
+            Student student3 = new Student("Bonita", "Applebum", "bonita@gamil.com");
             // start a transaction
             session.beginTransaction();
             // save the student object
-            session.save(student);
+            session.save(student1);
+            session.save(student2);
+            session.save(student3);
             System.out.println("Saving the student...");
             // commit the transaction
             session.getTransaction().commit();
