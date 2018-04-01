16db2bca5c5003895b6ba100f1d467c5b5193e2b
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-01 02:45:50 +0200

    hibernate advance mapping - one to one - prep work

--- src/main/java/com/gemalto/hb_01_one_to_one_uni/testjdbcconnection/TestJdbc.java
+++ src/main/java/com/gemalto/hb_01_one_to_one_uni/testjdbcconnection/TestJdbc.java
@@ -1,11 +1,11 @@
-package com.gemalto.testjdbcconnection;
+package com.gemalto.hb_01_one_to_one_uni.testjdbcconnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class TestJdbc {
     public static void main(String[] args) {
-        String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false";
         String user = "hbstudent";
         String pass = "hbstudent";
 
