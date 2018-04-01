bd75b9599b6abd6343497f4edf81fe06ff09334f
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-01 23:24:03 +0200

    hibernate advance mapping - eager vs lazy - hql join fetch

--- src/main/java/com/gemalto/hb_eager_vs_lazy/demo/FetchJoinDemo.java
+++ src/main/java/com/gemalto/hb_eager_vs_lazy/demo/FetchJoinDemo.java
@@ -7,8 +7,9 @@ import com.gemalto.hb_eager_vs_lazy.entity.InstructorDetail;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
-public class EagerLazyDemo {
+public class FetchJoinDemo {
 
     public static void main(String[] args) {
 
@@ -27,27 +28,31 @@ public class EagerLazyDemo {
             // start a transaction
             session.beginTransaction();
 
+            // option 2: Hibernate query with HQL
+
             // get instructor from db
             int id = 1;
-            Instructor instructor = session.get(Instructor.class, id);
+            Query<Instructor> query =
+                    session.createQuery("select i from Instructor i " +
+                                    "JOIN FETCH i.courses " + "where i.id=:instructorId",
+                            Instructor.class);
+            // set parameter on query
+            query.setParameter("instructorId", id);
+
+            // execute query and get instructor
+            Instructor instructor = query.getSingleResult();
+
             System.out.println("luv2code: Instrctor: " + instructor);
 
-            /*// get courses for the instructor
-            // also Solution:1 - call getter method while session is opened
-            System.out.println("luv2code: Courses: " + instructor.getCourses());*/
 
             // commit the transaction
             session.getTransaction().commit();
 
-            // close the session (purposely) - it will fail lazy loading below
+            // close the session
             session.close();
             System.out.println("\nluv2code: The session is now closed!\n");
 
-            // Solution:
-            // 1. call getter method while session is opened
-            // 2. HQL join fetch
-
-            // it will fail because session is closed above before lazy loading below
+            // get courses for instructor
             System.out.println("luv2code: Courses: " + instructor.getCourses());
 
             System.out.println("luv2code: Done!");
